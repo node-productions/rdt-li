a23127bf6df64ef07086ba87668ae5f242630eb0
sakunaga <sakunaga+github@nodepro.jp>
2024-10-15 19:57:00 +0900

    up

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,7 +23,10 @@ export function sanitize(text: string) {
 }
 
 export const getBaseURL = () => {
-  const url = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  const isProd = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+  const url = isProd
+    ? process.env.VERCEL_URL
+    : process.env.VERCEL_PROJECT_PRODUCTION_URL
 
   return url ? `https://${url}` : `http://localhost:${process.env.PORT || 3000}`
 }
